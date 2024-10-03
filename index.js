0846303012a2b34f735341a1766e1f19dc355b2a
BarunBlog <bhattacharjeebarun25@gmail.com>
2024-10-03 20:08:34 +0600

    Added /users endpoint

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-res.status(200).send(`Hello, from Node App on PORT: ${port}!`);
+app.get("/", (req, res) => {
+  res.status(200).send(`Hello, from Node App on PORT: ${port}!`);
+});
+
+app.get("/users", (req, res) => {
+  res.status(200).send(`Congratulations! You have reached the users endpoint!`);
 });
 
 app.listen(port, () => {
-console.log(`App running on http://localhost:${port}`);
+  console.log(`App running on http://localhost:${port}`);
 });
